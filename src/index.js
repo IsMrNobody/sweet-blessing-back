4b7ea0db95e4fc3c946a18ece1dbee1bff48cbc1
unknown <jose_millan_9@hotmail.com>
2022-09-07 19:49:02 -0400

    upload

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { PORT } from "./config";
 
 import payment from "./routes/paymentRouts";
-import webPushRouts from "./routes/webPushRouts";
+// import webPushRouts from "./routes/webPushRouts";
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use(payment);
-app.use(webPushRouts);
+// app.use(webPushRouts);
 
 app.listen(PORT);
 console.log("server on port", PORT)
