500bde4e1d04de90ff9b4378c49879010c07304e
unknown <jose_millan_9@hotmail.com>
2022-09-26 13:52:24 -0400

    prueba-header

--- dist/routes/paymentRouts.js
+++ dist/routes/paymentRouts.js
@@ -3,12 +3,11 @@ const { creatOrder, captureOrder, cancelOrder } = require('../controllers/paymem
 
 const router = Router()
 
-// router.post('/create-order', creatOrder)
+router.post('/create-order', creatOrder)
 
-router.post('/create-order', async (req, res) => {
-    const order =  await creatOrder(req.body)
-    // console.log(order)
-})
+// router.post('/create-order', async (req, res) => {
+//     const order =  await creatOrder(req.body)
+// })
 
 
 router.get('/capture-order', captureOrder)
