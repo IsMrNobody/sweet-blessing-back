cdb0afa46db7754f3a632d6bc46d566627189b14
unknown <jose_millan_9@hotmail.com>
2022-09-29 18:45:14 -0400

    log-msg

--- dist/routes/orderRouts.js
+++ dist/routes/orderRouts.js
@@ -21,6 +21,7 @@ router.get('/user/:id', async (req, res) => {
 })
 
 router.post("/create", async (req, res) => {
+    console.log(req.body)
     const order = await createOrder(req.body)
     if(order){
         res.status(201).json({data: order})
