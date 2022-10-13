b896831b53a93094d970b24978a37be65ce237af
unknown <jose_millan_9@hotmail.com>
2022-10-13 18:15:51 -0400

    update-merchantbyid

--- dist/controllers/merchantController.js
+++ dist/controllers/merchantController.js
@@ -1,50 +1,55 @@
 const Local = require("../models/merchant")
 
 const createMerchant = async (data) => {
-    try {
-        console.log(data);
-        const merchant = new Local(data)
-        await merchant.save()
-        return merchant
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    console.log(data);
+    const merchant = new Local(data)
+    await merchant.save()
+    return merchant
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 const getMerchants = () => {
-    const merchants = Local.find()
-    return merchants
+  const merchants = Local.find()
+  return merchants
 }
 
-const getMerchantById = (id) => {
-    const product = Local.findById(id)
-    return product
+const getMerchantById = async (id) => {
+  try {
+    const merchant = await Local.findById(id)
+    return merchant
+  } catch (error) {
+    return error.message
+  }
 }
 
 const editMerchant = async (id, data) => {
-    try {
-        await Local.findByIdAndUpdate(id, data)
-        const merchantAct = Local.findById(id)
-        return merchantAct
-    } catch (error) {
-        console.log('no encontrado');
-    }
+  try {
+    await Local.findByIdAndUpdate(id, data)
+    const merchantAct = Local.findById(id)
+    return merchantAct
+  } catch (error) {
+    console.log('no encontrado');
+    return error.message
+  }
 }
 
 const deleteMerchant = async (id) => {
-    try {
-        const merchant = await Local.findByIdAndDelete(id)
-        if (!merchant) throw new Error('no existe')
-        return merchant
-    } catch (error) {
-        return error.message
-    }
+  try {
+    const merchant = await Local.findByIdAndDelete(id)
+    if (!merchant) throw new Error('no existe')
+    return merchant
+  } catch (error) {
+    return error.message
+  }
 }
 
 module.exports = {
-    createMerchant,
-    getMerchants,
-    getMerchantById,
-    editMerchant,
-    deleteMerchant,
-  }
+  createMerchant,
+  getMerchants,
+  getMerchantById,
+  editMerchant,
+  deleteMerchant,
+}
--- dist/routes/merchantRouts.js
+++ dist/routes/merchantRouts.js
@@ -1,46 +1,49 @@
 const { Router } = require('express')
-const { createMerchant, 
-    getMerchants, 
-    editMerchant, 
-    deleteMerchant,
-    getMerchantById
+const { createMerchant,
+  getMerchants,
+  editMerchant,
+  deleteMerchant,
+  getMerchantById
 } = require('../controllers/merchantController')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const merchants = await getMerchants()
-    res.status(201).json(merchants)
-    console.log('obteniendo inf....')
+  const merchants = await getMerchants()
+  res.status(201).json(merchants)
+  console.log('obteniendo inf....')
 })
 
 router.get('/:id', async (req, res) => {
-    const merchant = await getMerchantById(req.params.id)
+  const merchant = await getMerchantById(req.params.id)
+  if (merchant._id) {
     res.status(201).json(merchant)
-    console.log('obteniendo inf....>')
+  } else {
+    res.status(403).json('algo ocurre > ' + merchant)
+  }
 })
 
 router.post("/create-local", async (req, res) => {
-    // console.log(req.body);
-    const merchant = await createMerchant(req.body)
-    if(merchant){
-        res.status(201).json({data: merchant})
-    } else {
-        res.status(403).json('algo ocurre > ' + merchant)
-    }
+  // console.log(req.body);
+  const merchant = await createMerchant(req.body)
+  if (merchant) {
+    res.status(201).json({ data: merchant })
+  } else {
+    res.status(403).json('algo ocurre > ' + merchant)
+  }
 })
 
 router.put('/:id', async (req, res) => {
-    const edit = await editMerchant(req.params.id, req.body)
-    res.status(201).json(edit)
+  const edit = await editMerchant(req.params.id, req.body)
+  res.status(201).json(edit)
 })
 
 router.delete("/:id", async (req, res) => {
-    const merchant = await deleteMerchant(req.params.id)
-    if(merchant === 'no existe') {
-        res.status(403).json('algo ocurre > ' + merchant)
-    } else {
-        res.status(201).json('eliminado')
-    }
+  const merchant = await deleteMerchant(req.params.id)
+  if (merchant === 'no existe') {
+    res.status(403).json('algo ocurre > ' + merchant)
+  } else {
+    res.status(201).json('eliminado')
+  }
 })
 
 module.exports = router
